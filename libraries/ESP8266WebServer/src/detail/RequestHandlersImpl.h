8664fa482679a38a828e6773f0bdb8c65059623e
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2020-10-27 10:39:41 +0100

    Add files via upload

--- libraries/ESP8266WebServer/src/detail/RequestHandlersImpl.h
+++ libraries/ESP8266WebServer/src/detail/RequestHandlersImpl.h
@@ -7,7 +7,7 @@
 #include "WString.h"
 #include "Uri.h"
 
-using namespace mime;
+namespace esp8266webserver {
 
 template<typename ServerType>
 class FunctionRequestHandler : public RequestHandler<ServerType> {
@@ -25,21 +25,21 @@ public:
         delete _uri;
     }
 
-    bool canHandle(HTTPMethod requestMethod, String requestUri) override  {
+    bool canHandle(HTTPMethod requestMethod, const String& requestUri) override  {
         if (_method != HTTP_ANY && _method != requestMethod)
             return false;
 
         return _uri->canHandle(requestUri, RequestHandler<ServerType>::pathArgs);
     }
 
-    bool canUpload(String requestUri) override  {
+    bool canUpload(const String& requestUri) override  {
         if (!_ufn || !canHandle(HTTP_POST, requestUri))
             return false;
 
         return true;
     }
 
-    bool handle(WebServerType& server, HTTPMethod requestMethod, String requestUri) override {
+    bool handle(WebServerType& server, HTTPMethod requestMethod, const String& requestUri) override {
         (void) server;
         if (!canHandle(requestMethod, requestUri))
             return false;
@@ -48,7 +48,7 @@ public:
         return true;
     }
 
-    void upload(WebServerType& server, String requestUri, HTTPUpload& upload) override {
+    void upload(WebServerType& server, const String& requestUri, HTTPUpload& upload) override {
         (void) server;
         (void) upload;
         if (canUpload(requestUri))
@@ -81,11 +81,11 @@ public:
             _isFile = false;
         }
 
-        DEBUGV("StaticRequestHandler: path=%s uri=%s isFile=%d, cache_header=%s\r\n", path, uri, _isFile, cache_header);
+        DEBUGV("StaticRequestHandler: path=%s uri=%s isFile=%d, cache_header=%s\r\n", path, uri, _isFile, cache_header == __null ? "" : cache_header);
         _baseUriLength = _uri.length();
     }
 
-    bool canHandle(HTTPMethod requestMethod, String requestUri) override  {
+    bool canHandle(HTTPMethod requestMethod, const String& requestUri) override  {
         if ((requestMethod != HTTP_GET) && (requestMethod != HTTP_HEAD))
             return false;
 
@@ -95,33 +95,38 @@ public:
         return true;
     }
 
-    bool handle(WebServerType& server, HTTPMethod requestMethod, String requestUri) override {
+    bool handle(WebServerType& server, HTTPMethod requestMethod, const String& requestUri) override {
+
         if (!canHandle(requestMethod, requestUri))
             return false;
 
         DEBUGV("StaticRequestHandler::handle: request=%s _uri=%s\r\n", requestUri.c_str(), _uri.c_str());
 
-        String path(_path);
+        String path;
+        path.reserve(_path.length() + requestUri.length() + 32);
+        path = _path;
 
         if (!_isFile) {
-            // Base URI doesn't point to a file.
-            // If a directory is requested, look for index file.
-            if (requestUri.endsWith("/"))
-              requestUri += "index.htm";
 
             // Append whatever follows this URI in request to get the file path.
             path += requestUri.substring(_baseUriLength);
 
+            // Base URI doesn't point to a file.
+            // If a directory is requested, look for index file.
+            if (path.endsWith("/"))
+                path += F("index.htm");
+
             // If neither <blah> nor <blah>.gz exist, and <blah> is a file.htm, try it with file.html instead
             // For the normal case this will give a search order of index.htm, index.htm.gz, index.html, index.html.gz
             if (!_fs.exists(path) && !_fs.exists(path + ".gz") && path.endsWith(".htm")) {
-                path += "l";
+                path += 'l';
             }
         }
         DEBUGV("StaticRequestHandler::handle: path=%s, isFile=%d\r\n", path.c_str(), _isFile);
 
         String contentType = mime::getContentType(path);
 
+        using namespace mime;
         // look for gz file, only if the original specified path is not a gz.  So part only works to send gzip via content encoding when a non compressed is asked for
         // if you point the the path to gzip you will serve the gzip as content type "application/x-gzip", not text or javascript etc...
         if (!path.endsWith(FPSTR(mimeTable[gz].endsWith)) && !_fs.exists(path))  {
@@ -160,5 +165,6 @@ protected:
     size_t _baseUriLength;
 };
 
+} // namespace
 
 #endif //REQUESTHANDLERSIMPL_H
